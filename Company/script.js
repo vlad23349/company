7fb78b0148edf802b387055b5099018e7107ccfd
Vladislav <vlad.kolesnik.99@list.ru>
2022-11-22 13:09:33 +0300

    refactor: change var to let

--- Company/script.js
+++ Company/script.js
@@ -1,10 +1,9 @@
-menu1.onclick = function myFunction(){
-    var x = document.getElementById("myTopNav");
+menu1.onclick = function myFunction() {
+  let x = document.getElementById("myTopNav");
 
-    if(x.className === "topNav"){
-        x.className += " responsive";
-    }
-    else {
-        x.className = "topNav";
-    }
-}
+  if (x.className === "topNav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topNav";
+  }
+};
